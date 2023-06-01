ca8f1ec8bbd303c6a69be92285a876b5ab63b760
jotyy <jotyy318@gmail.com>
2023-06-01 22:44:58 +0800

    chore: use webp

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         xs: '450px',
       },
       backgroundImage: {
-        'hero-pattern': "url('/img/pattern.png')",
+        'hero-pattern': "url('/img/pattern.webp')",
       },
     },
   },

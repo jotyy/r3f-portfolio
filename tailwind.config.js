06ad8dcce325c562f86e04c166bc03df4968f7e8
jotyy <jotyy318@gmail.com>
2023-06-01 14:23:03 +0800

    feat: update icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: '450px',
       },
       backgroundImage: {
-        'hero-pattern': "url('/img/herobg.png')",
+        'hero-pattern': "url('/img/herobg.webp')",
       },
     },
   },

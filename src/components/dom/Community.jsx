131f7f3b2c6c09e9a8186a7a4dacb0472746fea8
jotyy <jotyy318@gmail.com>
2023-08-09 19:55:01 +0800

    feat: new x icon

--- src/components/dom/Community.jsx
+++ src/components/dom/Community.jsx
@@ -8,14 +8,18 @@ const Community = () => {
           xmlns='http://www.w3.org/2000/svg'
           width='24'
           height='24'
-          viewBox='0 0 24 24'
+          viewBox='-2 -2 24 24'
           fill='none'
           stroke='currentColor'
           stroke-width='2'
           stroke-linecap='round'
           stroke-linejoin='round'
         >
-          <path d='M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z' />
+          <path
+            fill='currentColor'
+            stroke='none'
+            d='M16.99 0H20.298L13.071 8.26L21.573 19.5H14.916L9.702 12.683L3.736 19.5H0.426L8.156 10.665L0 0H6.826L11.539 6.231L16.99 0ZM15.829 17.52H17.662L5.83 1.876H3.863L15.829 17.52Z'
+          />
         </svg>
       </a>
       <a href='https://github.com/jotyy' target='_blank' aria-label='github link' className='community-icon'>
